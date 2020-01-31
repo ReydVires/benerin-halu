0cb32991a946a8700d9e274a6662ce018bd2b980
Ahmad Arsyel <ahmad.fire@gmail.com>
2020-02-01 04:25:14 +0700

    add blend tree

--- Benerin Halu/Assets/Scripts/People.cs
+++ Benerin Halu/Assets/Scripts/People.cs
@@ -7,17 +7,13 @@ public class People : MonoBehaviour, IMoveable
     public float speed;
     public float sanityMeter;
     Rigidbody2D rb2d;
-    public int facing;
-    public Animator anim;
+    Animator anim;
     
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        var random = new Random();
-        // Error
-        facing = random.Next(-1, 1);
-        Debug.Log(facing);
+        anim.SetFloat("XPos", 1.0f);
     }
 
     void Update()
