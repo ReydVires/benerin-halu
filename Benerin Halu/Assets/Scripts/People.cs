e3af4635381c10e799940cb8db7ca6e218a36ef9
Ahmad Arsyel <ahmad.fire@gmail.com>
2020-02-01 04:04:17 +0700

    add some bug

---
+++ Benerin Halu/Assets/Scripts/People.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class People : MonoBehaviour, IMoveable
+{
+    public float speed;
+    public float sanityMeter;
+    Rigidbody2D rb2d;
+    public int facing;
+    public Animator anim;
+    
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        var random = new Random();
+        // Error
+        facing = random.Next(-1, 1);
+        Debug.Log(facing);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Move()
+    {
+        
+    }
+}
