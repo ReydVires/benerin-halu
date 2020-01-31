de2e3aad7314fcc61fab7733bf3fc19f53e2b952
Ahmad Arsyel <ahmad.fire@gmail.com>
2020-02-01 06:53:32 +0700

    add new anim

--- Benerin Halu/Assets/Scripts/People.cs
+++ Benerin Halu/Assets/Scripts/People.cs
@@ -6,6 +6,7 @@ public class People : MonoBehaviour, IMoveable
 {
     public float speed;
     public float sanityMeter;
+    public bool facingRight = true;
     Rigidbody2D rb2d;
     Animator anim;
     
@@ -13,7 +14,19 @@ public class People : MonoBehaviour, IMoveable
     {
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        anim.SetFloat("XPos", 1.0f);
+        initFacing();
+    }
+
+    void initFacing()
+    {
+        if (facingRight)
+        {
+            anim.SetFloat("XPos", 1.0f);
+        }
+        else
+        {
+            anim.SetFloat("XPos", -1.0f);
+        }
     }
 
     void Update()
